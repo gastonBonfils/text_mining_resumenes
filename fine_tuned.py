cb2df913644d9b29bbe43e359a155fbcdcf221e7
Gaston Bonfils <gastonbonfils@mi.unc.edu.ar>
2024-10-14 21:13:09 -0300

    pruebas iniciales

---
+++ fine_tuned.py
@@ -0,0 +1,21 @@
+from transformers import pipeline
+
+# summarizer = pipeline(
+#     "summarization", model="kabita-choudhary/finetuned-bart-for-conversation-summary"
+# )
+
+
+# # texto = ""
+# # with open("./ejemplo2.txt", "r", encoding="utf-8") as chat:
+# #     texto += chat.read()
+
+# # print(summarizer(texto[:1024], max_length=1024, min_length=30, do_sample=False))
+
+
+def summarize_chat(chat):
+    summarizer = pipeline(
+        "summarization",
+        model="kabita-choudhary/finetuned-bart-for-conversation-summary",
+    )
+    summary = summarizer(chat[:1024], max_length=1024, min_length=30, do_sample=False)
+    return summary
